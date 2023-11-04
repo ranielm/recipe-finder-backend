dd75364e69defbd23eb5dca02305aca89032f8bd
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-11-04 00:18:54 -0300

    adding bcryptjs and jsonwebtoken

--- src/routes/recipeRoutes.ts
+++ src/routes/recipeRoutes.ts
@@ -1,20 +1,14 @@
 import express from 'express';
-import {
-  getAllRecipes,
-  getRecipe,
-  createRecipe,
-  updateRecipe,
-  deleteRecipe,
-  searchRecipesByIngredients,
-} from '../controllers/recipeController';
+
+import { RecipeIController } from '../controllers/recipeController';
 
 const router = express.Router();
 
-router.get('/search', searchRecipesByIngredients);
-router.get('/', getAllRecipes);
-router.get('/:id', getRecipe);
-router.post('/', createRecipe);
-router.put('/:id', updateRecipe);
-router.delete('/:id', deleteRecipe);
+router.get('/search', RecipeIController.searchRecipesByIngredients);
+router.get('/', RecipeIController.getAllRecipes);
+router.get('/:id', RecipeIController.getRecipe);
+router.post('/', RecipeIController.createRecipe);
+router.put('/:id', RecipeIController.updateRecipe);
+router.delete('/:id', RecipeIController.deleteRecipe);
 
 export default router;
