40f1be8a773c7003e080a252170a92636b092eb9
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-11-06 00:23:22 -0300

    update docs

--- docs/post-ingredients.tsx
+++ docs/post-ingredients.tsx
@@ -3,7 +3,6 @@
   "description": "A versatile type of flour used for general baking purposes."
 }
 
-
 {
   "name": "Granulated White Sugar",
   "description": "Common sugar used in baking and cooking to sweeten dishes."
@@ -14,3 +13,17 @@
   "description": "A leavening agent used in baking to help dough rise."
 }
 
+{
+  "name": "Rolled Oats",
+  "description": "Whole grain oats that are steamed and rolled to reduce cooking time, ideal for baking and breakfast cereals."
+}
+
+{
+  "name": "Brown Sugar",
+  "description": "A type of sugar with a moist texture and a rich molasses flavor, commonly used in baking."
+}
+
+{
+  "name": "Ground Cinnamon",
+  "description": "A sweet and woody spice obtained from the inner bark of several tree species from the genus Cinnamomum, used for flavoring in a variety of dishes."
+}
